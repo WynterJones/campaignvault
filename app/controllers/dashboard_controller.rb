6a3678fb218ae5db637098a760b5244685a2793a
monetizedesign <wynter@monetizedesign.com>
2019-11-14 02:45:03 -0500

    add campaigns to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -21,10 +21,13 @@ class DashboardController < ApplicationController
     @timeframe = timeframe(params[:timeframe])
     requests = Request.all
     apps = App.all
+    campaigns = Campaign.all
     @all_requests = number_with_delimiter(requests.where('created_at >= ?', @timeframe).count)
     @all_apps = number_with_delimiter(apps.where('created_at >= ?', @timeframe).count)
+    @all_campaigns = number_with_delimiter(campaigns.where('created_at >= ?', @timeframe).count)
     @all_requests_total = number_with_delimiter(requests.count)
     @all_apps_total = number_with_delimiter(apps.count)
+    @all_campaigns_total = number_with_delimiter(campaigns.count)
     @all_activity = requests.where('created_at >= ?', @timeframe).group_by_day(:created_at).count
   end
 end
