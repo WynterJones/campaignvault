a493e7d99ab804fc58676b3e0a33d5910c8997d8
monetizedesign <wynter@monetizedesign.com>
2019-11-15 00:57:43 -0500

    popup slide in sidebar

--- app/helpers/json_helper.rb
+++ app/helpers/json_helper.rb
@@ -6,32 +6,34 @@ module JsonHelper
       return false
   end
 
-  def parse_json_for_tooltip(hash, iteration = 0, request = '', settings = '')
+  def parse_json_for_sidebar(hash, iteration = 0, request = '', settings = '')
     output = ""
     iteration += 1
     hash.each do |key, value|
       padding = "padding-left: #{iteration - 1}em;"
       if value.is_a?(Hash)
         output += "<span class='tooltip-info' style='#{padding}'><strong>#{key.capitalize()}:</strong></span><br />"
-        output += parse_json_for_tooltip(value,iteration)
+        output += parse_json_for_sidebar(value,iteration)
       elsif value.is_a?(Array)
         output += "<span class='tooltip-info' style='#{padding}'><strong>#{key.capitalize()}:</strong></span>"
         value.each do |value|
           if value.is_a?(String) then
             output += "<span class='tooltip-info' style='#{padding}'>#{value}</span>"
           else
-            output += parse_json_for_tooltip(value,iteration-1)
+            output += parse_json_for_sidebar(value,iteration-1)
           end
         end
       else
         if iteration > 1
-          output += "<span class='tooltip-info' style='#{padding}'><strong>#{key.capitalize()}</strong><br>#{value}</span>"
+          output += "<span class='table-row-info'><strong>#{key.capitalize()}</strong><br>#{value}</span>"
         else
-          if key != '' && value != ''
-            output += "<div class='card-block mb-2'><strong class='possible-table-key'>#{key}</strong><span class='float-right'>#{value}</span></div>"
-          elsif key != '' && value == ''
-            output += "<div class='card-block mb-2'><strong class='possible-table-key'>#{key}</strong><span class='float-right' style='opacity: 0.2'>null</span></div>"
+          if key != '' && value == ''
+            value = 'null'
           end
+          output += "<div class='table-row-info'>
+                      <strong class='popup-table-key float-right'>#{key}</strong>
+                      <span class='popup-table-value'>#{value}</span>
+                    </div>"
         end
       end
     end
--- app/javascript/custom/app-popup.js
+++ app/javascript/custom/app-popup.js
@@ -6,6 +6,40 @@ const appPopup = {
     if ($(element).index() != 0) {
       let template = $(element).parent().attr('data-template')
       $('#row-data-popup').html($(`#${template}`).html())
+
+
+      var list, i, switching, b, shouldSwitch;
+        list = document.getElementById("row-data-popup");
+        switching = true;
+        /* Make a loop that will continue until
+        no switching has been done: */
+        while (switching) {
+          // Start by saying: no switching is done:
+          switching = false;
+          b = list.getElementsByTagName("DIV");
+          // Loop through all list items:
+          for (i = 0; i < (b.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Check if the next item should
+            switch place with the current item: */
+            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+              /* If next item is alphabetically lower than current item,
+              mark as a switch and break the loop: */
+              shouldSwitch = true;
+              break;
+            }
+          }
+          if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark the switch as done: */
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+          }
+        }
+
+
+
       $('#popup-wrapper').fadeIn(200)
       $('#popup').attr('style', 'right: 0')
     }
