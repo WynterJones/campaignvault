c5611080919e9c3bed619db7a5030346514870e3
monetizedesign <wynter@monetizedesign.com>
2019-11-26 02:58:26 -0500

    users

--- app/workers/save_request.rb
+++ app/workers/save_request.rb
@@ -18,7 +18,7 @@ class SaveRequest
       campaign = Campaign.find_by_slug(campaign_slug)
       app = App.find_by(campaign_id: campaign.id, slug: app_slug)
       database = Database.find_by(app_id: app.id, slug: database_slug)
-      Request.create(data: newhash, values: newarray, database_id: database.id)
+      Request.create(data: newhash, values: newarray, database_id: database.id, user_id: database.user_id)
       if database.connected == false
         database.connected = true
         database.save
---
+++ db/migrate/20191126055526_add_user_id_again_to_requests.rb
@@ -0,0 +1,5 @@
+class AddUserIdAgainToRequests < ActiveRecord::Migration[6.0]
+  def change
+    add_column :requests, :user_id, :integer
+  end
+end
